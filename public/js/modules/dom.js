71ac258bb28a0aa96c0456c702d2ffa6b2aaeb51
Radv2005 <U202312318@upc.edu.pe>
2025-04-21 23:19:55 -0500

    feat: add testimonial section to landing page

--- public/js/modules/dom.js
+++ public/js/modules/dom.js
@@ -20,4 +20,31 @@ export function updateTexts(data) {
     document.getElementById("hero-title").innerHTML = data.hero.title; // usa innerHTML para <span>
     document.getElementById("hero-description").textContent = data.hero.description;
     document.getElementById("cta-btn").textContent = data.hero.cta;
+
+    document.getElementById("testimonial-title").textContent = data.testimonialTitle;
+    //testimonial
+    const testimonialElements = [
+        document.getElementById("testimonial-1"),
+        document.getElementById("testimonial-2"),
+        document.getElementById("testimonial-3")
+    ];
+    data.testimonials.forEach((testimonialData, index) => {
+        const testimonialElement = testimonialElements[index];
+        // Actualizr text del testimonio
+        testimonialElement.querySelector(".testimonial-text").textContent = testimonialData.text;
+        // Actualizar nombre
+        testimonialElement.querySelector(".testimonial-name").textContent = testimonialData.name;
+        // actualizar posición
+        testimonialElement.querySelector(".testimonial-position").textContent = testimonialData.position;
+        // actualizar imagen
+        testimonialElement.querySelector(".testimonial-img").src = testimonialData.image;
+        testimonialElement.querySelector(".testimonial-img").alt = testimonialData.name;
+        // Estrellas
+        const rating = testimonialData.rating || 5;
+        const fullStars = "★".repeat(rating);
+        const emptyStars = "☆".repeat(5 - rating);
+        testimonialElement.querySelector(".testimonial-rating").textContent = fullStars + emptyStars;
+
+
+    });
 }
