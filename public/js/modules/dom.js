ddd4655d93a19765abbb60deb2cce6aff2623f92
diegovilcasaboya <diegovilcatut@gmail.com>
2025-04-23 18:53:40 -0500

    feat: add section benefits with i18n

--- public/js/modules/dom.js
+++ public/js/modules/dom.js
@@ -20,4 +20,16 @@ export function updateTexts(data) {
     document.getElementById("hero-title").innerHTML = data.hero.title; // usa innerHTML para <span>
     document.getElementById("hero-description").textContent = data.hero.description;
     document.getElementById("cta-btn").textContent = data.hero.cta;
+
+    //Benefits
+    document.getElementById("benefits-subtitle").textContent =data.benefits.subtitle;
+    document.getElementById("benefits-title").textContent =data.benefits.title;
+    document.getElementById("benefits-description").textContent=data.benefits.description;
+    document.getElementById("benefits-subtitle1").textContent =data.benefits.subtitle1;
+    document.getElementById("benefits-description1").textContent =data.benefits.description1;
+    document.getElementById("benefits-subtitle2").textContent =data.benefits.subtitle2;
+    document.getElementById("benefits-description2").textContent =data.benefits.description2;
+    document.getElementById("benefits-subtitle3").textContent =data.benefits.subtitle3;
+    document.getElementById("benefits-description3").textContent =data.benefits.description3;
+
 }
